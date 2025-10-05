f0698c250e7aa997eb3e7073a146bcb6a39925f6
NisagiPathirana <kpndomalsa19@gmail.com>
2025-10-05 12:26:50 +0530

    update DashBoard UI

--- src/main/java/edu/icet/controller/DashBoardController.java
+++ src/main/java/edu/icet/controller/DashBoardController.java
@@ -1,4 +1,41 @@
 package edu.icet.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashBoardController {
+
+    @FXML
+    private JFXButton btnCustomerManagement;
+
+    @FXML
+    private JFXButton btnItemManagement;
+
+    @FXML
+    private JFXButton btnOrderDetailManagement;
+
+    @FXML
+    private JFXButton btnOrderManagement;
+
+    @FXML
+    void btnCustomerManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderManagementOnAction(ActionEvent event) {
+
+    }
+
 }
